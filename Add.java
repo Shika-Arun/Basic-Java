2060479616a9127f4668689a427ea2a025bfecd4
Shika-Arun <shikaarun10097@gmail.com>
2026-02-24 13:50:25 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,8 +1,7 @@
-Class Add {
+public class Add {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        System.out.println(a+b);
+        System.out.println(a + b);
     }
 }
-
